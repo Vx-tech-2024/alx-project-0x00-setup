a5bf3a21e027d221b3adcc852774a3a680fe1115
Vx-tech <xaviervianney8@gmail.com>
2025-11-14 22:49:55 +0300

    Making the button function edited version 4th  editing

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,5 +1,6 @@
-import Card from "@/components/Card"
-import Button  from "@/components/Button"
+import Button  from "@/components/Button";
+import Card from "@/components/Card";
+
 const Landing: React.FC =  () => {
   return (
     <div>
